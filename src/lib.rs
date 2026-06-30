4a29cd2bcacd179bcac98d912b0b7256533aad7b
Vi Ramen <vi.is.chapmann@gmail.com>
2026-07-01 01:32:25 +0300

    Initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -43,69 +43,111 @@ unsafe impl core::marker::Sync for Export {}
 unsafe impl core::marker::Send for Import {}
 unsafe impl core::marker::Sync for Import {}
 
-pub const fn parse_version(s:&str)->u64{let bytes=s.as_bytes();let len=bytes.len();let mut major=0u32;
-let mut minor=0u32;let mut dot_pos=len;let mut i=0;while i<len{if bytes[i]==b'.'{dot_pos = i;break
-}i+=1}let mut j=0;while j<dot_pos{let digit=bytes[j];if digit<b'0'||digit>b'9'{panic!("invalid")}
-major=major*10+(digit-b'0')as u32;j+=1;}if dot_pos<len{let mut k=dot_pos+1;while k<len{let digit=
-bytes[k];if digit<b'0'||digit>b'9'{return 0;}minor=minor*10+(digit-b'0')as u32;k+=1;}}(major as
-u64)<<32|minor as u64}
+pub const fn parse_version(s: &str) -> u64 {
+    let bytes = s.as_bytes();
+    let len = bytes.len();
+    let mut dot_pos = len;
+    let (mut major, mut minor, mut i, mut j) = (0, 0, 0, 0);
+
+    while i < len {
+        if bytes[i] == b'.' {
+            dot_pos = i;
+            break
+        }
+        i += 1
+    }
+    while j < dot_pos {
+        let digit = bytes[j];
+        if digit < b'0' || digit > b'9' { panic!("invalid") }
+        major = major * 10 + (digit - b'0') as u32;
+        j+=1
+    }
+    if dot_pos < len {
+        let mut k = dot_pos + 1;
+        while k < len {
+            let digit = bytes[k];
+            if digit < b'0' || digit > b'9' {
+                return 0
+            }
+            minor = minor * 10 + ( digit - b'0') as u32;
+            k+=1
+        }
+    }
+    
+    (major as u64) << 32 | minor as u64
+}
 
 #[macro_export]
 macro_rules! Import {
-    ($i:ident => $n:ident, since kernel $x:literal) => {
+    ( $i:ident => $n:ident, since kernel $x:literal ) => {
         paste!(
-        #[used]
-        #[allow(non_upper_case_globals)]
+            #[used]
+            #[allow(non_upper_case_globals)]
             #[unsafe(export_name = concat!("_Ki", stringify!($n)))]
-        static [< Ki $n >]: $crate::Import = $crate::Import($i as *const (), parse_version(stringify!($x)));
+            static [< Ki $n >]: $crate::Import = $crate::Import($i as *const (), parse_version(stringify!($x)));
         );
     };
-    ($n:ident, since $x:literal) => {
+
+    ( $n:ident, since $x:literal ) => {
         paste!(
-        #[used]
-        #[allow(non_upper_case_globals)]
+            #[used]
+            #[allow(non_upper_case_globals)]
             #[unsafe(export_name = concat!("_Ki", stringify!($n)))]
-        static [< Ki $n >]: $crate::Import = $crate::Import($n as *const (), parse_version(stringify!($x)));
+            static [< Ki $n >]: $crate::Import = $crate::Import($n as *const (), parse_version(stringify!($x)));
         );
     };
-    ($i:ident => $n:ident, since $x:literal) => {
+    ( $i:ident => $n:ident, since $x:literal ) => {
         paste!(
-        #[used]
-        #[allow(non_upper_case_globals)]
+            #[used]
+            #[allow(non_upper_case_globals)]
             #[unsafe(export_name = concat!("_Mi", stringify!($n)))]
-        static [< Mi $n >]: $crate::Import = $crate::Import($i as *const (), parse_version(stringify!($x)));
+            static [< Mi $n >]: $crate::Import = $crate::Import($i as *const (), parse_version(stringify!($x)));
         );
     };
-    ($n:ident, since $x:literal) => {
+    ( $n:ident, since $x:literal ) => {
         paste!(
-        #[used]
-        #[allow(non_upper_case_globals)]
+            #[used]
+            #[allow(non_upper_case_globals)]
             #[unsafe(export_name = concat!("_Mi", stringify!($n)))]
-        static [< Mi $n >]: $crate::Import = $crate::Import($n as *const (), parse_version(stringify!($x)));
+            static [< Mi $n >]: $crate::Import = $crate::Import($n as *const (), parse_version(stringify!($x)));
         );
     };
-    ( $vis:vis fn $n:ident ( $($name:ident : $aty:ty),* ) $( -> $rty:ty )? where kernel $x:literal $b:block ) => {
+    ( $(#[$attr:meta])* $vis:vis fn $n:ident ( $($name:ident : $aty:ty),* ) $( -> $rty:ty )? where kernel $x:literal $b:block ) => {
         paste!(
             #[allow(non_snake_case)]
             fn [< __stub_ $n >]( $( $name : $aty ),* ) $( -> $rty )? { $b }
+
             #[used]
             #[allow(non_upper_case_globals)]
             #[unsafe(export_name = concat!("Ki", stringify!($n)))]
             static [< _ $n >]: Import = Import([< __stub_ $n >] as *const (), parse_version(stringify!($x)));
-            #[allow(non_snake_case)] #[inline(always)]
-            $vis fn [< $n >]( $( $name : $aty ),* ) $( -> $rty )? { (unsafe{([< _ $n >].0 as *const fn ( $( $name : $aty ),* ) $( -> $rty )?).as_ref_unchecked()})( $( $name ),* )}
+
+            $(#[$attr])*
+            #[allow(non_snake_case)]
+            #[inline(always)]
+            $vis fn [< $n >]( $( $name : $aty ),* ) $( -> $rty )? {
+                (unsafe{([< _ $n >].0 as *const fn ( $( $name : $aty ),* ) $( -> $rty )?).as_ref_unchecked()})
+                ( $( $name ),* )
+            }
         );
     };
-    ( $vis:vis fn $n:ident ( $($name:ident : $aty:ty),* ) $( -> $rty:ty )? where $x:literal $b:block ) => {
+    ( $(#[$attr:meta])* $vis:vis fn $n:ident ( $($name:ident : $aty:ty),* ) $( -> $rty:ty )? where $x:literal $b:block ) => {
         paste!(
             #[allow(non_snake_case)]
             fn [< __stub_ $n >]( $( $name : $aty ),* ) $( -> $rty )? { $b }
+
             #[used]
             #[allow(non_upper_case_globals)]
             #[unsafe(export_name = concat!("Mi", stringify!($n)))]
-            $vis static [< _ $n >]: Import = Import([< __stub_ $n >] as *const (), parse_version(stringify!($x)));
-            #[allow(non_snake_case)] #[inline(always)]
-            $vis fn [< $n >]( $( $name : $aty ),* ) $( -> $rty )? { (unsafe{([< _ $n >].0 as *const fn ( $( $name : $aty ),* ) $( -> $rty )?).as_ref_unchecked()})( $( $name ),* )}
+            static [< _ $n >]: Import = Import([< __stub_ $n >] as *const (), parse_version(stringify!($x)));
+
+            $(#[$attr])*
+            #[allow(non_snake_case)]
+            #[inline(always)]
+            $vis fn [< $n >]( $( $name : $aty ),* ) $( -> $rty )? {
+                (unsafe{([< _ $n >].0 as *const fn ( $( $name : $aty ),* ) $( -> $rty )?).as_ref_unchecked()})
+                ( $( $name ),* )
+            }
         );
     };
 }
@@ -140,23 +182,25 @@ macro_rules! Export {
         static [< Me $n >]: $crate::Export = $crate::Export($n as *const (), parse_version(stringify!($x)));
         );
     };
-    ( $vis:vis fn $n:ident ( $($name:ident : $aty:ty),* ) $( -> $rty:ty )? where kernel $x:literal $b:block ) => {
+    ( $(#[$attr:meta])* $vis:vis fn $n:ident ( $($name:ident : $aty:ty),* ) $( -> $rty:ty )? where kernel $x:literal $b:block ) => {
         paste!(
             #[allow(non_snake_case)]
             fn [< __stub_ $n >]( $( $name : $aty ),* ) $( -> $rty )? { $b }
+
             #[used]
             #[allow(non_upper_case_globals)]
-            #[unsafe(export_name = concat!("Ke", stringify!($n)))]
+            #[unsafe(export_name = concat!("Ke", stringify!($n)))] $(#[$attr])*
             $vis static $n: Export = Export([< __stub_ $n >] as *const (), parse_version(stringify!($x)));
         );
     };
-    ( $vis:vis fn $n:ident ( $($name:ident : $aty:ty),* ) $( -> $rty:ty )? where $x:literal $b:block ) => {
+    ( $(#[$attr:meta])* $vis:vis fn $n:ident ( $($name:ident : $aty:ty),* ) $( -> $rty:ty )? where $x:literal $b:block ) => {
         paste!(
             #[allow(non_snake_case)]
             fn [< __stub_ $n >]( $( $name : $aty ),* ) $( -> $rty )? { $b }
+            
             #[used]
             #[allow(non_upper_case_globals)]
-            #[unsafe(export_name = concat!("Me", stringify!($n)))]
+            #[unsafe(export_name = concat!("Me", stringify!($n)))] $(#[$attr])*
             $vis static $n: Export = Export([< __stub_ $n >] as *const (), parse_version(stringify!($x)));
         );
     };
