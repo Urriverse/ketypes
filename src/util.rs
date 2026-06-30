42e6fb30cae49d45ebd5e7f530adb3740b8d3853
Vi Ramen <vi.is.chapmann@gmail.com>
2026-07-01 01:44:55 +0300

    Initial commit

--- src/util.rs
+++ src/util.rs
@@ -8,8 +8,6 @@ use core::ptr::addr_of;
 pub use core::{alloc::Layout, fmt::Arguments, panic::PanicInfo};
 pub use alloc::{boxed::Box, collections::btree_map::BTreeMap, string::String, vec::Vec, sync::Arc};
 
-pub(crate) macro Ke ( $( $(#[$attr:meta])* $n:ident @ $ty:ty )+ ) { $( $(#[$attr])* pub type $n = $ty; )+ }
-
 #[repr(align(1))]  // explicitly unaligned
 pub struct KeAbstract<const N: usize>([u8; N]);
 
